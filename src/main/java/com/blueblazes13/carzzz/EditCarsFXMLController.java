22d82c674d362afa03f687fae4afc59d187443bb
blueblazes13 <blueblazes13@gmail.com>
2021-07-20 23:32:25 +0200

    Major car editor changes

--- src/main/java/com/blueblazes13/carzzz/EditCarsFXMLController.java
+++ src/main/java/com/blueblazes13/carzzz/EditCarsFXMLController.java
@@ -19,6 +19,7 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -35,8 +36,11 @@ public class EditCarsFXMLController {
     private URL location;
 
     @FXML
-    private AnchorPane apItemField;
+    private HBox hbItemField;
 
+    @FXML
+    private TextField tfName;
+    
     @FXML
     private MenuItem btnClose;
 
@@ -51,12 +55,12 @@ public class EditCarsFXMLController {
 
     @FXML
     private MenuItem btnAdd;
-    
-    @FXML
-    private Label lblTitle;
 
     @FXML
     private Button btnBack;
+    
+    @FXML
+    private Button btnDelete;
     
     @FXML
     private VBox vbLabels;
@@ -68,8 +72,10 @@ public class EditCarsFXMLController {
     @FXML
     void initialize() {
         CarzzzModel.setController("EditCarsFXML", this);
+        this.model = CarzzzModel.getModel().getSelectedBrand();
         
         this.btnBack.setOnAction(this::back);
+        this.btnDelete.setOnAction(this::delete);
         this.ivCarImage.setOnMouseClicked((MouseEvent me) -> {
             File image = CarzzzModel.showImageChooser();
             if (image != null) this.ivCarImage.setImage(CarzzzModel.fileToImage(image));
@@ -81,14 +87,19 @@ public class EditCarsFXMLController {
     
     
     public void update() {
-        this.model = CarzzzModel.getModel().getSelectedBrand();
-        
         if (this.model.getSelectedCar() == null) {
             this.carModel = new CarModel("No name set");
             this.model.addCar(this.carModel);
             this.model.selectCar(this.carModel);
         } else {
             this.carModel = this.model.getSelectedCar();
+        }
+        this.tfName.setText(this.carModel.getName());
+        
+        if (this.carModel.getCarImage()== null) {
+            this.ivCarImage.setImage(CarzzzModel.stringToImage("clickToAdd.png"));
+        } else {
+            this.ivCarImage.setImage(CarzzzModel.stringToImage(this.carModel.getCarImage()));
         }
         
         this.vbLabels.getChildren().clear();
@@ -99,16 +110,19 @@ public class EditCarsFXMLController {
         
         Label title1 = new Label("Name");
         title1.setPrefSize(87, 35);
-        title1.textFillProperty().setValue(Color.WHITE);
+        title1.textFillProperty().setValue(Color.BLACK);
         title1.setPrefSize(87, 35);
         title1.setFont(Font.font("TechnicLite", 14));
-        title1.setTextFill(Color.WHITE);
+        title1.setTextFill(Color.BLACK);
         title1.setAlignment(Pos.BASELINE_LEFT);
 
         TextField infoField1 = new TextField();
+        infoField1.setStyle("-fx-background-color: rgb(240, 240, 240)");
+        infoField1.setPrefWidth(180);
         infoField1.setText(this.carModel.getName());
         infoField1.textProperty().addListener((ov, t, t1) -> {
             this.carModel.setName(t1);
+            this.tfName.setText(t1);
             labels.renameCar(t, t1);
         });
 
@@ -120,13 +134,15 @@ public class EditCarsFXMLController {
         for (String label: carLabels) {
             Label title = new Label(label);
             title.setPrefSize(87, 35);
-            title.textFillProperty().setValue(Color.WHITE);
+            title.textFillProperty().setValue(Color.BLACK);
             title.setPrefSize(87, 35);
             title.setFont(Font.font("TechnicLite", 14));
-            title.setTextFill(Color.WHITE);
+            title.setTextFill(Color.BLACK);
             title.setAlignment(Pos.BASELINE_LEFT);
             
             TextField infoField = new TextField();
+            infoField.setStyle("-fx-background-color: rgb(240, 240, 240)");
+            infoField.setPrefWidth(180);
             infoField.setText(CarzzzModel.infoLabels.getCarData(this.carModel.getName()).get(label));
             infoField.textProperty().addListener((ov, t, t1) -> {
                 CarzzzModel.getModel().infoLabels.setCarData(this.carModel.getName(), label, t1);
@@ -139,10 +155,15 @@ public class EditCarsFXMLController {
     }
 
     
-    
     private void back(ActionEvent ae) {
         this.model.deselectCar();
         ((CarsMenuFXMLController)CarzzzModel.getController("CarsMenuFXML")).update();
         ScreenController.changeScreen("CarsMenuFXML");
+    }
+    
+    
+    private void delete(ActionEvent ae) {
+        this.model.removeCar(this.carModel);
+        this.back(ae);
     }
 }
